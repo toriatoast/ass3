38202f3fd6420097829db37356deefecae7a8713
toriatoast <victoria.inthavong@gmail.com>
2023-10-26 22:42:53 +1100

    dob and dropdown and submit

--- validation.js
+++ validation.js
@@ -22,19 +22,32 @@ var namechar = /^[a-zA-Z-,]+(\s{0,1}[ '.\a-zA-Z-, ])*$/;
 //valid date format
 var dateformat = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
 
-// adding event listener upon clicking on the input field and/or submitting
- form.addEventListener('change', e => {
-     e.preventDefault(); //prevent the form submitting before validating the inputs
+//setting up valid date format function
+function isValidDate(dateString)
+{
+    // First check for the pattern
+    if(!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString))
+        return false;
 
-     validateInputs(); //validate inputs function
+    // Parse the date parts to integers
+    var parts = dateString.split("/");
+    var day = parseInt(parts[1], 10);
+    var month = parseInt(parts[0], 10);
+    var year = parseInt(parts[2], 10);
 
- });
+    // Check the ranges of month and year
+    if(year < 1000 || year > 3000 || month == 0 || month > 12)
+        return false;
 
- form.addEventListener('submit', l => {
-     l.preventDefault(); //prevent the form submitting before validating the inputs
+    var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
 
-     validateInputs(); //validate inputs function
- });
+    // Adjust for leap years
+    if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+        monthLength[1] = 29;
+
+    // Check the range of the day
+    return day > 0 && day <= monthLength[month - 1];
+};
 
 //setting error message
 // based on the error provided which is input control and save a reference for the input display which is inside inputControl as a div  
@@ -68,49 +81,74 @@ const validateInputs = () => {
     const dobValue=dob.value.trim();
 
     if(emailValue === '') {
-        setError(email, 'Email is required');
+        setError(email, 'Email is required'); 
     } else if (!isValidEmail(emailValue)) {
-        setError(email, 'Provide a valid email address');
+        setError(email, 'Provide a valid email address'); 
     } else {
-        setSuccess(email);
+        setSuccess(email); 
     }
 
     if(passwordValue === '') {
-        setError(password, 'Password is required');
+        setError(password, 'Password is required'); 
     } else if (password.value.match(passw)) {
-        setSuccess(password);
+        setSuccess(password); 
     } else {
-        setError(password, 'Password must contain 8 characters, and include number, upper case, and lower case letter.');
+        setError(password, 'Password must contain 8 characters, and include number, upper case, and lower case letter.'); 
     }
 
     if(password2Value === '') {
-        setError(password2, 'Please confirm your password');
+        setError(password2, 'Please confirm your password'); 
     } else if (password2Value !== passwordValue) {
-        setError(password2, 'Passwords do not match');
+        setError(password2, 'Passwords do not match'); 
     } else {
-        setSuccess(password2);
+        setSuccess(password2); 
     }
 
     if(fnameValue === '') {
-        setError(fname, 'First name is required');
+        setError(fname, 'First name is required'); 
     } else if (fname.value.match(namechar)) {
-        setSuccess(fname)
+        setSuccess(fname); 
     } else {
         setError(fname, 'First name should only include letters, apostrophes, spaces, and hypens');
     }
 
     if(lnameValue === '') {
-        setError(lname, 'Last name is required');
+        setError(lname, 'Last name is required'); 
     } else if (lname.value.match(namechar)) {
-        setSuccess(lname)
+        setSuccess(lname);
     } else {
-        setError(lname, 'Last name should only include letters, apostrophes, spaces, and hypens');
+        setError(lname, 'Last name should only include letters, apostrophes, spaces, and hyphens');
+    }
+ 
+    if (dobValue === '') {
+        setError(dob, 'Date of Birth is required');
+    } else if (!isValidDate(dobValue)) {
+        setError(dob, 'Invalid Date of Birth format or date');
+    } else {
+        setSuccess(dob);
     }
 
-    if(dobValue==='') {
-        setError(dob, 'Enter valid date');
-    } else (dobValue.value.match(dateformat)); {
-        setSuccess(dob)
-    } 
-    
 };
+
+// Create an array of input elements
+const inputElements = [email, password, password2, fname, lname, dob];
+
+// Add event listeners to each input element
+inputElements.forEach((inputElement) => {
+    inputElement.addEventListener('change', () => {
+        validateInputs(inputElement);
+    });
+});
+
+
+//Add event listener for when submit button is clicked
+form.addEventListener('submit', l => {
+    l.preventDefault(); //prevent the form submitting before validating the inputs
+
+    validateInputs(); //validate inputs function
+
+    if (document.querySelectorAll('.success').length === 6) {
+        // submit the form
+        form.submit();
+    } 
+ });
